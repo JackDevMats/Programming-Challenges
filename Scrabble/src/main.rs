a0667b3574d294afcce197f935908155e7e1abf0
JackDevMats <jackatmast@gmail.com>
2017-04-08 17:37:23 -0400

    Bonus 1 implemented

--- Scrabble/src/main.rs
+++ Scrabble/src/main.rs
@@ -22,6 +22,15 @@ fn main() {
         println!("Your word, {}, cannot be made with the available characters.",
                  &input.available_chars);
     }
+
+    //Bonus 1
+    if check_chars_bonus_1(&input.available_chars, &input.word) {
+        println!("Bonus 1:Your word, {}, can be made with the available characters.",
+                 &input.available_chars);
+    } else {
+        println!("Bonus 1:Your word, {}, cannot be made with the available characters.",
+                 &input.available_chars);
+    }
 }
 
 fn get_input() -> Result<ScrabbleInput, std::io::Error> {
@@ -51,12 +60,36 @@ fn check_chars(input_chars: &String, word: &String) -> bool {
     true
 }
 
+//Bonus 1
 fn check_chars_bonus_1(input_chars: &String, word: &String) -> bool {
     let mut find_question_marks = input_chars.clone();
-    for char in word.chars() {
+    let mut letters_left = word.clone();
+    for (index, char) in word.chars().enumerate() {
         match input_chars.find(char) {
-            None => return false,
-            _ => ()
+            None => (),
+            Some(i) => {
+                find_question_marks.remove(i);
+                let index = letters_left.find(char);
+                letters_left.remove(index.unwrap());
+                ()
+            }
         }
     }
+    //Is there enough free letters to make the word
+    if number_of_question_marks(input_chars) == letters_left.len() as i32 {
+        true
+    } else {
+        false
+    }
+}
+
+//Finds number of free letters that can be used to create the input word
+fn number_of_question_marks(input: &String) -> i32 {
+    let mut count:i32 = 0;
+    for char in input.chars() {
+        if &*char.to_string() == "?" {
+            count +=1;
+        }
+    }
+    count
 }
